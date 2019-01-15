bd13dea998f4ad5a8c5a6c5f1f1e85f819ea9d74
Fuyunwang <2375872953@qq.com>
2019-01-15 18:41:11 +0800

    mybatis逆向工程

---
+++ youxin-store-parent/youxin-store-dao/src/main/java/com/beautifulsoup/youxin/mapper/TbItemCatMapper.java
@@ -0,0 +1,30 @@
+package com.beautifulsoup.youxin.mapper;
+
+import com.beautifulsoup.youxin.dataobject.TbItemCat;
+import com.beautifulsoup.youxin.dataobject.TbItemCatExample;
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
+
+public interface TbItemCatMapper {
+    int countByExample(TbItemCatExample example);
+
+    int deleteByExample(TbItemCatExample example);
+
+    int deleteByPrimaryKey(Long id);
+
+    int insert(TbItemCat record);
+
+    int insertSelective(TbItemCat record);
+
+    List<TbItemCat> selectByExample(TbItemCatExample example);
+
+    TbItemCat selectByPrimaryKey(Long id);
+
+    int updateByExampleSelective(@Param("record") TbItemCat record, @Param("example") TbItemCatExample example);
+
+    int updateByExample(@Param("record") TbItemCat record, @Param("example") TbItemCatExample example);
+
+    int updateByPrimaryKeySelective(TbItemCat record);
+
+    int updateByPrimaryKey(TbItemCat record);
+}
